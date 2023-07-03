9c3db06d20e1d60c00ce0bdad2a532b0b259ec3f
Pietro-SA <115328178+Pietro-SA@users.noreply.github.com>
2023-07-04 00:26:43 +0200

    version 2.0.0
    - fix: guessing one letter will affect the entire array, and not only the current position of it
    - extra: commented part of the code to help understanding

--- src/entities/Hangman.java
+++ src/entities/Hangman.java
@@ -4,12 +4,21 @@ public class Hangman {
 
 	private int lives = 5;
 	private char[] word;
-	private int currentPosition = 0;
+	private char[] guessing;
+	private boolean isCoincidence;
+	private int charsLeft;
+	
+	/*
+	 * Constructor for Hangman class. It also builds the 'word' char array, which contains all the letters that have to be guessed.
+	 */
 	
 	public Hangman(String word) {
 		this.word = new char[word.length()];
+		guessing = new char[word.length()];
+		charsLeft = word.length();
 		for (int i = 0; i < word.length(); i++) {
 			this.word[i] = word.charAt(i);
+			guessing[i] = ' ';
 		}
 	}
 	
@@ -21,8 +30,42 @@ public class Hangman {
 		return word;
 	}
 	
-	public void checkLetter(char letra) {
-		if (word[currentPosition] == letra ) {
+	/*
+	 * NEW SYSTEM: Uses a 'for' loop to go through the 'word' array and look for any coincidences with the provided letter/char.
+	 * If there aren't any coincidences, subtract 'lives' by 1 and prints the remaining lives. In case the value is lower than 0, print "you lost!", but always calls 'printHangman()' method.
+	 */
+	
+	public void checkLetter(char letter) {
+		isCoincidence = false;
+		for (int i = 0; i < word.length; i++) {
+			if (guessing[i] == letter) {
+				System.out.println("Please choose a different letter.");
+				isCoincidence = true;
+				break;
+			} else if (word[i] == letter) {
+				isCoincidence = true;
+				guessing[i] = letter;
+				charsLeft--;
+			}
+		}
+		if (!isCoincidence) {
+			if (lives > 0) {
+				lives--;
+				System.out.println("Wrong answer! you have " + lives + " extra attempts left.");
+			} else {
+				System.out.println("Game over!");
+			}
+			printHangman();
+			
+		}
+	}
+	
+	/*
+	 * DEPRECATED: Old letter verification system
+	 * It can only check one char at a time!
+	 
+	public void checkLetter(char letter) {
+		if (word[currentPosition] == letter ) {
 			currentPosition++;
 			printHangman();
 		} else {
@@ -35,22 +78,18 @@ public class Hangman {
 			printHangman();
 		}
 	}
+	*/
 	
 	public boolean endGame() {
-		return currentPosition == word.length;
+		return charsLeft == 0;
 	}
 	
 	public void printClue() {
-		if (currentPosition > 0) {
-			for(int i = 0; i < currentPosition; i++) {
-				System.out.print(word[i]);
-			}
-			for(int i = 0; i < word.length - currentPosition; i++) {
-				System.out.print("_");
-			}
-		} else {
 			for(int i = 0; i < word.length; i++) {
-				System.out.print("_");
+				if (guessing[i] != ' ') {
+					System.out.print(word[i]);	
+				} else {
+					System.out.print("_");
 			}
 		}
 	}
